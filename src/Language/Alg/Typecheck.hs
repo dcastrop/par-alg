7301226c96e218b77b438d5ea347f0221bcbc3d7
dcastrop <d.castro-perez@imperial.ac.uk>
2018-11-09 13:08:35 +0000

    Solved space leak in typechecking

--- src/Language/Alg/Typecheck.hs
+++ src/Language/Alg/Typecheck.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -20,6 +21,7 @@ module Language.Alg.Typecheck
 import Control.Arrow ( (***) )
 import Control.Monad ( zipWithM )
 import qualified Data.Set as Set
+import Data.List ( foldl1' )
 import qualified Data.Map.Strict as Map
 import Control.Monad ( foldM, (<=<) )
 import Data.Text.Prettyprint.Doc hiding ( annotate )
@@ -40,8 +42,7 @@ import Language.Par.Prog
 type SEnv t = (Env (Func t), Env (Type t))
 
 typecheck :: Prim v t => Int -> St t -> Prog t v -> IO (TcSt t, TyEnv t, AProg t v)
-typecheck d st = (\((e, p), tcst) -> return (tcst, e, p))
-               <=< (go . checkProg)
+typecheck d st p = go (checkProg p) >>= \((e, p'), tcst) -> return (tcst, e, p')
   where
     go = runTcM d st
 
@@ -130,16 +131,16 @@ typeOf s e@(Case es   ) t
       ts  <- mapM (const (TMeta <$> fresh)) es
       to  <- TMeta <$> fresh
       s'  <- unify s t (TSum ts Nothing `tFun` to)
-      foldM (uncurry . typeOf) s' $ zip es $ map (`tFun` to) ts
+      foldM (uncurry . typeOf) s' $! zip es $! map (`tFun` to) ts
 typeOf s (Fmap f e   ) t = do
   a <- TMeta <$> fresh
   b <- TMeta <$> fresh
   s' <- unify s t (TApp f a `tFun` TApp f b)
   typeOf s' e (a `tFun` b)
 typeOf s (In f       ) t =
-  unify s t $ TApp f (TRec f) `tFun` TRec f
+  unify s t $! TApp f (TRec f) `tFun` TRec f
 typeOf s (Out f      ) t =
-  unify s t $ TRec f `tFun` TApp f (TRec f)
+  unify s t $! TRec f `tFun` TApp f (TRec f)
 typeOf s (Rec f e1 e2) t = do
   a <- TMeta <$> fresh
   b <- TMeta <$> fresh
@@ -156,10 +157,10 @@ unifyPoly s p          x@(PMeta i)
   | i `Set.member` metaVars p = fail $ "Occurs check, cannot unify '"
                                 ++ render x ++ "' with '" ++ render p ++ "'"
   | Just p' <- Map.lookup i (fst s) = unifyPoly s p p'
-  | otherwise = pure $ (Map.insert i p $ fst s, snd s)
+  | otherwise = pure $! (Map.insert i p $! fst s, snd s)
 unifyPoly s x@PMeta{}  p = unifyPoly s p x
-unifyPoly s (PPrd ps1) (PPrd ps2) = foldM (uncurry . unifyPoly) s $ zip ps1 ps2
-unifyPoly s (PSum ps1) (PSum ps2) = foldM (uncurry . unifyPoly) s $ zip ps1 ps2
+unifyPoly s (PPrd ps1) (PPrd ps2) = foldM (uncurry . unifyPoly) s $! zip ps1 ps2
+unifyPoly s (PSum ps1) (PSum ps2) = foldM (uncurry . unifyPoly) s $! zip ps1 ps2
 unifyPoly s (PK t1)    (PK t2)    = unify s t1 t2
 unifyPoly s t1    t2
   | t1 == t2   = return s
@@ -214,21 +215,21 @@ unify s t x@(TMeta i)
   | Just t' <- Map.lookup i (snd s)  = unify s t t'
   | i `Set.member` metaVars t = fail $ "Occurs check, cannot unify '"
                                 ++ render x ++ "' with '" ++ render t ++ "'"
-  | otherwise                 = pure $ (id *** Map.insert i t) s
+  | otherwise                 = pure $! (id *** Map.insert i t) s
 unify s x@TMeta{} t = unify s t x
-unify s0 (TFun ts1) (TFun ts2) = foldM (uncurry . unify) s0 $ zipF ts1 ts2
+unify s0 (TFun ts1) (TFun ts2) = foldM (uncurry . unify) s0 $! zipF ts1 ts2
 unify s0 t1@(TSum ts1 mii) t2@(TSum ts2 mjj)
   = do s1 <- foldM (uncurry . unify) s0 ts
        unifyTail msge TSum s1 m mii mjj
   where
-    (ts, m) = zipD ts1 ts2
+    !(ts, m) = zipD ts1 ts2
     msge _ = "Cannot unify sum '" ++ render t1
               ++ "' with '" ++ render t2 ++ "'"
 unify s0 t1@(TPrd ts1 mii) t2@(TPrd ts2 mjj)
   = do s1 <- foldM (uncurry . unify) s0 ts
        unifyTail msge TPrd s1 m mii mjj
   where
-    (ts, m) = zipD ts1 ts2
+    !(ts, m) = zipD ts1 ts2
     msge s' = "Cannot unify prod '" ++ render (subst s' t1)
               ++ "' with '" ++ render (subst s' t2) ++ "'"
 unify s0 (TApp f1 t1) (TApp f2 t2) = do
@@ -256,7 +257,7 @@ unifyTail :: (Eq t, Pretty t, IsCompound t)
           -> Maybe Int
           -> Maybe Int
           -> TcM t (SEnv t)
-unifyTail _      _   s None Nothing  Nothing  = return s
+unifyTail _      _   s None Nothing  Nothing  = return $! s
 unifyTail _      spr s None (Just i) mj       = unify s (spr [] mj) (TMeta i)
 unifyTail _      spr s None mi (Just j)       = unify s (spr [] mi) (TMeta j)
 unifyTail _      spr s (JLeft l) mi (Just j)  = unify s (spr l  mi) (TMeta j)
@@ -280,19 +281,19 @@ roleInfer (AnnAlg e r) t = do
   ty <- TMeta <$> fresh
   let (ri, ti) = rGet t
   s  <- tcTerm e (ti `tFun` ty)
-  return $ addBranch ri $ TyAnn (subst s ty) r
+  return $! addBranch ri $ TyAnn (subst s ty) r
 roleInfer (AnnComp es) t = go (reverse es) t
   where
     go [] t' = pure t'
     go (x:xs) t' = roleInfer x t' >>= go xs
 roleInfer p@(AnnPrj i) (TyPrd ts)
   | length ts > (fromInteger i)
-  = pure $ ts !! (fromInteger i)
+  = pure $! ts !! (fromInteger i)
   | otherwise
   = fail $ "Cannot infer annotated type of '" ++ render p ++ "'"
 roleInfer p@(AnnPrj i) (TyAnn (TPrd ts _) r)
   | length ts > (fromInteger i)
-  = pure $ TyAnn (ts !! (fromInteger i)) r
+  = pure $! TyAnn (ts !! (fromInteger i)) r
   | otherwise
   = fail $ "Cannot infer annotated type of '" ++ render p ++ "'"
 roleInfer (AnnSplit es) t
@@ -308,7 +309,7 @@ roleInfer (AnnCase es) (TyAnn (TSum ts _) r)
 roleInfer (AnnInj i) t
   = do vs <- mapM (const (TMeta <$> fresh)) [0..i-1]
        v <- fresh
-       pure $ TyBrn (fromInteger i) vs t (Left v) -- XXX: generate metavars!!!
+       pure $! TyBrn (fromInteger i) vs t (Left v) -- XXX: generate metavars!!!
 roleInfer AnnFmap{} _
   = error "Panic! fmap should be unrolled before reaching this point"
 roleInfer e (TyAnn (TApp f a) r)
@@ -319,29 +320,28 @@ roleInfer e t
 
 infixl 4 |>
 
-(|>) :: Pretty t => GTy t -> Global t -> String -> GTy t
+(|>) :: Pretty t => GTy t -> Global t -> GTy t
 Choice r rs gs1 |> g@(Brn g2)
-  = \ctx -> Choice r (Set.toList rs') $ mapAlt (\ (Lbl l) gt -> (gt |> g2 !! l) ctx) gs1
+  = Choice r lrs $! mapAlt (\ (Lbl l) gt -> gt |> g2 !! l) gs1
   where
-    rs' = Set.fromList rs `Set.union` gRoles g Set.\\ Set.singleton r
-Comm m g1       |> g2          = \ctx -> Comm m $ (g1 |> g2) ctx
-GRec v g1       |> g2          = \ctx -> GRec v $ (g1 |> g2) ctx
-g@GVar{}        |> _           = \_tx -> g
-GEnd            |> Leaf g2     = \_tx -> g2
-l               |> r           = \ctx -> error (m ctx)
-  where
-    m ctx = "Panic! Ill-formed sequencing of \
-        \global types in Language.Alg.Typecheck.|>, called from " ++ ctx ++ "\n\n"
-        ++ render l ++ "\n" ++ render r
-
-seqP :: Pretty t => String -> Global t -> Global t -> Global t
-seqP ctx (Leaf g1)  g2       = Leaf $ (g1 |> g2) ctx
-seqP ctx (Brn gs1) (Brn gs2) = Brn $ zipWith (seqP ctx) gs1 gs2
-seqP ctx l        r          = error m
+    !rs' = Set.fromList rs `Set.union` gRoles g Set.\\ Set.singleton r
+    !lrs = Set.toList rs'
+Comm m g1       |> g2          = Comm m $! g1 |> g2
+GRec v g1       |> g2          = GRec v $! g1 |> g2
+g@GVar{}        |> _           = g
+GEnd            |> Leaf g2     = g2
+_               |> _           = error m
   where
     m = "Panic! Ill-formed sequencing of \
-        \global types in Language.Alg.Typecheck.seqP called from " ++ ctx ++ "\n\n"
-        ++ render l ++ "\n" ++ render r
+        \global types in Language.Alg.Typecheck.|>"
+
+seqP :: Pretty t => Global t -> Global t -> Global t
+seqP (Leaf g1)  g2       = Leaf $! g1 |> g2
+seqP (Brn gs1) (Brn gs2) = Brn $! zipWith seqP gs1 gs2
+seqP _        _          = error m
+  where
+    m = "Panic! Ill-formed sequencing of \
+        \global types in Language.Alg.Typecheck.seqP"
 
 msg :: Pretty t => AType t -> RoleId -> TcM t (Global t)
 msg (TyAnn t ri   ) ro = Leaf <$> comm
@@ -349,7 +349,7 @@ msg (TyAnn t ri   ) ro = Leaf <$> comm
     comm = Comm <$> pure (Msg [ri] [ro] t ()) <*> pure GEnd
 msg (TyBrn _ _ a _) ro = msg a ro
 msg (TyAlt ts     ) ro = Brn <$> mapM (`msg` ro) ts
-msg t@(TyPrd ts   ) ro = foldr1 (seqP $ "msg:" ++ render t) <$> mapM (`msg` ro) ts
+msg (TyPrd ts     ) ro = foldl1' seqP <$> mapM (`msg` ro) ts
 msg t@TyApp{}       _  = fail $ "Found functor application: " ++ render t
 msg (TyMeta i)      _  = fail $ "Ambiguous annotated type" ++ render i
 
@@ -364,35 +364,20 @@ protoInfer (TyAnn (TApp f a) ri) e
 protoInfer ti    (AnnComp es ) = go (reverse es) ti
   where
     go [] t = protoInfer t (AnnId :: ATerm t v)
-    go (e:es') t = do
-      g  <- protoInfer t e
-      tn <- roleInfer e t
-      gg <- go es' tn
-      seqP ("protoInfer: comp: left: "
-            ++ render e
-            ++ "\n\n right:"
-            ++ render (AnnComp es')
-            ++ "\n\n type in:"
-            ++ render t
-            ++ "\n\n type next:"
-            ++ render tn
-            ++ "\n\n global type left \n\t"
-            ++ render g
-            ++ "\n\n global type right \n\t"
-            ++ render gg) <$> protoInfer t e <*> (roleInfer e t >>= go es')
-protoInfer ti ei@(AnnSplit es  ) = go es ti
+    go (e:es') t = seqP <$> protoInfer t e <*> (roleInfer e t >>= go es')
+protoInfer ti (AnnSplit es  ) = go es ti
   where
     go l (TyAlt ts) = Brn <$> mapM (go l) ts
-    go [] t = pure $ repeatAlts t $ Leaf GEnd
+    go [] t = pure $! repeatAlts t $ Leaf GEnd
     go (e:es') t
-      = seqP ("protoInfer: split: " ++ render ei)
+      = seqP
       <$> protoInfer t e
       <*> (roleInfer e t >>= \ tn ->
               go es' (multAlts tn t))
 protoInfer (TyAnn (TSum ts _) ri) (AnnCase es)
   = do gs <- zipWithM getGT as es
        let rs = Set.toList $ Set.unions (map getRoles gs) Set.\\ Set.singleton ri
-       return $ Leaf $ Choice ri rs $ foldr (uncurry addAlt) emptyAlt $ zip [0..] gs
+       return $! Leaf $! Choice ri rs $! foldr (uncurry addAlt) emptyAlt $! zip [0..] gs
   where
     as = map (`TyAnn` ri) ts
     getGT a e = protoInfer a e >>= \(Leaf i) -> pure i
