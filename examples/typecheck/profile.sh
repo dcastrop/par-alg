c2292f9852ec5c074e350d548412b8546c53fadd
dcastrop <d.castro-perez@imperial.ac.uk>
2018-11-12 22:32:47 +0000

    Profile

--- examples/typecheck/profile.sh
+++ examples/typecheck/profile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# stack --work-dir .stack-work-profile clean
 stack --work-dir .stack-work-profile --profile build --ghc-options="-ddump-stg -ddump-to-file"
 
 stack exec --work-dir .stack-work-profile -- par-lang $2 +RTS $1 -p
+
+hp2ps par-lang.hp -e8in -c
