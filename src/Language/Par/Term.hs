7eaf92dbb75bce5626334065720970509d719e6c
dcastrop <d.castro-perez@imperial.ac.uk>
2018-11-20 19:13:54 +0000

    FIXME in progress: f@r1 ||| g@r1

--- src/Language/Par/Term.hs
+++ src/Language/Par/Term.hs
@@ -5,7 +5,6 @@ module Language.Par.Term
   , annotate
   ) where
 
-
 import Control.Monad.RWS.Strict
 import Data.Set ( Set )
 import qualified Data.Set as Set
@@ -37,19 +36,54 @@ termRoles AnnPrj{}        = Set.empty
 termRoles AnnInj{}        = Set.empty
 termRoles (AnnFmap _ r _) = roleIds r
 
+-- annotate :: (Pretty v, Pretty t, Ord v, Ord t) => RoleId -> Set (Alg t v) -> Alg t v -> TcM t v (ATerm t v)
+-- annotate r s = ann
+--   where
+--     ann t
+--       | t `Set.member` s = AnnAlg t <$!> newRole
+--     ann (Comp es    ) = AnnComp <$!> mapM ann es
+--     ann  Id           = pure AnnId
+--     ann (Proj i  j  ) = pure $! AnnPrj i j
+--     ann (Split es   ) = AnnSplit <$!> mapM ann es
+--     ann (Inj i   j  ) = pure $ AnnInj i j
+--     ann (Case es    ) = AnnCase <$!> (mapM ann es)
+--     ann (Fmap f e   ) = appPoly f e >>= ann
+--     ann t             = pure $! AnnAlg t r
+
+  --- XXX: Fixme: alternative with single role and no interactions
 annotate :: (Pretty v, Pretty t, Ord v, Ord t) => RoleId -> Set (Alg t v) -> Alg t v -> TcM t v (ATerm t v)
-annotate r s = ann
+annotate ri s tm = snd <$!> ann (RId ri) tm
   where
-    ann t
-      | t `Set.member` s = AnnAlg t <$!> newRole
-    ann (Comp es    ) = AnnComp <$!> mapM ann es
-    ann  Id           = pure AnnId
-    ann (Proj i  j  ) = pure $! AnnPrj i j
-    ann (Split es   ) = AnnSplit <$!> mapM ann es
-    ann (Inj i   j  ) = pure $ AnnInj i j
-    ann (Case es    ) = AnnCase <$!> (mapM ann es)
-    ann (Fmap f e   ) = appPoly f e >>= ann
-    ann t             = pure $! AnnAlg t r
+    ann _ t
+      | t `Set.member` s = (\r -> (RId r, AnnAlg t r)) <$!> newRole
+    ann r (Comp es    ) = go [] r (reverse es)
+      where
+        go acc rn [] = pure (rn, AnnComp acc)
+        go acc rn (t : ts) = do
+          (r1, x)  <- ann rn t
+          go (x:acc) r1 ts
+    ann r  Id           = pure (r, AnnId)
+    ann r (Proj i  j  ) = pure (prjR r, AnnPrj i j)
+      where
+        prjR (RPrd ts) = ts !! i
+        prjR rn        = rn
+    ann r (Split es   ) = ((\(a, b) -> (rPrd a, annSplit b)) . unzip) <$!> mapM (ann r) es
+    ann r (Inj i   j  ) = pure (RBrn i j r, AnnInj i j)
+    ann r (Case es    ) = ((\(a, b) -> (rAlt a, annCase b)) . unzip) <$!> mapM (ann r) es
+      where
+        rAlt r1@(x@RId{}:xs)
+          | all (== x) xs = x
+          | otherwise = RAlt r1
+        annCase ts@((AnnAlg _ ri) : xs)
+          | allAnnAlg ri xs =
+
+    ann r (Fmap f e   ) = appPoly f e >>= ann r
+    ann r t             = pure (RId $ oneOf r, AnnAlg t (oneOf r))
+      where
+        oneOf (RId rid) = rid
+        oneOf (RPrd xs) = oneOf $ last xs
+        oneOf (RAlt xs) = oneOf $ last xs
+        oneOf (RBrn _ _ x) = oneOf x
 
 appPoly :: (Pretty t, Pretty v) => Func t -> Alg t v -> TcM t v (Alg t v)
 appPoly  PK{}     _ = pure Id
