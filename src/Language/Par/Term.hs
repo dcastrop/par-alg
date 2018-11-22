54a0b97a6a5ac243d329da7afd090e07af7fefed
dcastrop <d.castro-perez@imperial.ac.uk>
2018-11-22 18:47:04 +0000

    Role selection strategies

--- src/Language/Par/Term.hs
+++ src/Language/Par/Term.hs
@@ -58,49 +58,46 @@ annSplit = annT Split AnnSplit
 annCase :: [ATerm t v] -> ATerm t v
 annCase = annT Case AnnCase
 
-annotate :: (Pretty v, Pretty t, Ord v, Ord t) => RoleId -> Set (Alg t v) -> Alg t v -> TcM t v (ATerm t v)
-annotate r s = ann
-  where
-    ann t
-      | t `Set.member` s = AnnAlg t <$!> newRole
-    ann (Comp es    ) = annComp <$!> mapM ann es
-    ann  Id           = pure AnnId
-    ann (Proj i  j  ) = pure $! AnnPrj i j
-    ann (Split es   ) = annSplit <$!> mapM ann es
-    ann (Inj i   j  ) = pure $ AnnInj i j
-    ann (Case es    ) = annCase <$!> (mapM ann es)
-    ann (Fmap f e   ) = appPoly f e >>= ann
-    ann t             = pure $! AnnAlg t r
-
----- XXX: Fixme: alternative with single role and no interactions
 --annotate :: (Pretty v, Pretty t, Ord v, Ord t) => RoleId -> Set (Alg t v) -> Alg t v -> TcM t v (ATerm t v)
---annotate ri s tm = snd <$!> ann (RId ri) tm
+--annotate r s = ann
 --  where
---    ann _ t
---      | t `Set.member` s = (\r -> (RId r, AnnAlg t r)) <$!> newRole
---    ann r (Comp es    ) = go [] r (reverse es)
---      where
---        go acc rn [] = pure (rn, annComp acc)
---        go acc rn (t : ts) = do
---          (r1, x)  <- ann rn t
---          go (x:acc) r1 ts
---    ann r  Id           = pure (r, AnnId)
---    ann r (Proj i  j  ) = pure (prjR r, AnnPrj i j)
---      where
---        prjR (RPrd ts) = ts !! i
---        prjR rn        = rn
---    ann r (Split es   ) = ((\(a, b) -> (rPrd a, annSplit b)) . unzip) <$!> mapM (ann r) es
---
---    ann r (Inj i   j  ) = pure (RBrn i j r, AnnInj i j)
---    ann r (Case es    ) = ((\(a, b) -> (rAlt a, annCase b)) . unzip) <$!> mapM (ann r) es
---
---    ann r (Fmap f e   ) = appPoly f e >>= ann r
---    ann r t             = pure (RId $ oneOf r, AnnAlg t (oneOf r))
---      where
---        oneOf (RId rid) = rid
---        oneOf (RPrd xs) = oneOf $ last xs
---        oneOf (RAlt xs) = oneOf $ last xs
---        oneOf (RBrn _ _ x) = oneOf x
+--    ann t
+--      | t `Set.member` s = AnnAlg t <$!> newRole
+--    ann (Comp es    ) = annComp <$!> mapM ann es
+--    ann  Id           = pure AnnId
+--    ann (Proj i  j  ) = pure $! AnnPrj i j
+--    ann (Split es   ) = annSplit <$!> mapM ann es
+--    ann (Inj i   j  ) = pure $ AnnInj i j
+--    ann (Case es    ) = annCase <$!> (mapM ann es)
+--    ann (Fmap f e   ) = appPoly f e >>= ann
+--    ann t             = pure $! AnnAlg t r
+
+-- XXX: Fixme: alternative with single role and no interactions
+annotate :: (Pretty v, Pretty t, Ord v, Ord t) => RoleId -> Set (Alg t v) -> Alg t v -> TcM t v (ATerm t v)
+annotate ri s tm = snd <$!> ann (RId ri) tm
+  where
+    ann _ t
+      | t `Set.member` s = (\r -> (RId r, AnnAlg t r)) <$!> newRole
+    ann r (Comp es    ) = go [] r (reverse es)
+      where
+        go acc rn [] = pure (rn, annComp acc)
+        go acc rn (t : ts) = do
+          (r1, x)  <- ann rn t
+          go (x:acc) r1 ts
+    ann r  Id           = pure (r, AnnId)
+    ann r (Proj i  j  ) = pure (prjR r, AnnPrj i j)
+      where
+        prjR (RPrd ts) = ts !! i
+        prjR rn        = rn
+    ann r (Split es   ) = ((\(a, b) -> (rPrd a, annSplit b)) . unzip) <$!> mapM (ann r) es
+
+    ann r (Inj i   j  ) = pure (RBrn i j r, AnnInj i j)
+    ann r (Case es    ) = ((\(a, b) -> (rAlt a, annCase b)) . unzip) <$!> mapM (ann r) es
+
+    ann r (Fmap f e   ) = appPoly f e >>= ann r
+    ann r t             = pure (RId $ oneOf r, AnnAlg t (oneOf r))
+      where
+        oneOf rr = head $ (filter (/= ri) $ Set.toList $ roleIds rr) ++ [ri]
 
 appPoly :: (Pretty t, Pretty v) => Func t -> Alg t v -> TcM t v (Alg t v)
 appPoly  PK{}     _ = pure Id
