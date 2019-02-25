42402e4a30e1a18994bf38d3af2a522dc2c64ee4
dcastrop <d.castro-perez@imperial.ac.uk>
2019-02-25 14:27:19 +0000

    Measurements

---
+++ examples/bench/FFT/plotter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./plot.py speedups Base/Measurements/FFT_seq.time K0/Measurements/FFT_par.time K1/Measurements/FFT_par.time K2/Measurements/FFT_par.time K3/Measurements/FFT_par.time K4/Measurements/FFT_par.time K5/Measurements/FFT_par.time K6/Measurements/FFT_par.time K7/Measurements/FFT_par.time K8/Measurements/FFT_par.time
